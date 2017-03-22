b93c0358c71bd311616c564ec1bb36bc0cc38c76
PixelZero <joris@lambooy.nl>
2017-03-22 18:15:12 +0100

    Uploading the project

---
+++ Assets/Scripts/CameraBehaviour.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour
+{
+    
+    public Transform playerTransform;
+
+    public float mouseSensitivity;
+    public float cameraDistance;
+
+    public bool invertX, invertY;
+    //float distance, fov;
+    float anglePitch = 0, angleYaw = 0; // Pitch = Up-Down; Yaw = Left/Right
+    
+	void Update ()
+    {
+        Vector2 mouseMovement = new Vector2 (Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+
+        float invX = -1, invY = -1;
+
+        if (invertX)
+            invX = 1;
+        if (invertY)
+            invY = 1;
+
+        // Fairly straight forward. Time.deltatime is commented out because i am not sure whether it is needed or not
+        angleYaw   += invX * mouseMovement.x * (mouseSensitivity / 10); // * Time.deltaTime;
+        anglePitch += invY * mouseMovement.y * (mouseSensitivity / 10); // * Time.deltaTime;
+
+        // Avoid to big numbers
+        if (angleYaw > 360)
+            angleYaw -= 360;
+        if (angleYaw < -360)
+            angleYaw += 360;
+
+        // Prevent the camera from tilting over/under the player (weird glitches otherwise)
+        if (anglePitch > 90)
+            anglePitch = 90;
+        if (anglePitch < -45)
+            anglePitch = -45;
+        
+        CalculatePosition();
+    }
+
+    // Calculate the camera offset to the player
+    void CalculatePosition()
+    {
+        // Convert to radians
+        float radPitch = Mathf.Deg2Rad * anglePitch;
+        float radYaw = Mathf.Deg2Rad * angleYaw;
+
+        // Horizontal distance (X and Z coordinates) is derived from the camera pitch
+        // Higher pitch -> camera looks down -> horizontal distance minimal
+        float horDistance = cameraDistance * Mathf.Cos(radPitch);
+        float y = 4 + cameraDistance * Mathf.Sin(radPitch);
+
+        // Calculate horizontal camera position
+        float x = horDistance * Mathf.Cos(radYaw);
+        float z = horDistance * Mathf.Sin(radYaw);
+        
+        // Apply the calulated offset
+        Vector3 offset = new Vector3(x, y, z);
+        this.transform.position = playerTransform.transform.position + offset;
+
+        // Make the camera look at the rocket
+        this.transform.rotation = Quaternion.Euler(Mathf.Sin(radPitch) * 90, -angleYaw - 90, 0);
+    }
+}
---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    public Rigidbody rigidbody;
+
+    public float thrusterForce;
+    public float steeringForce;
+
+    public float fuel;
+    public float fuel_consumption;
+
+	void Update ()
+    {
+        // For now, just test the flying-phase
+        Phase_Fly();
+	}
+
+    private void Phase_Launch()
+    {
+        // Still needs to be implemented properly
+    }
+
+    private void Phase_Fly()
+    {
+        if (fuel > 0 && Input.GetKey(KeyCode.Space))
+        {
+            // Always thrust forward ("up" from the rocket's POV)
+            Vector3 thrusterDirection = rigidbody.transform.up;
+            rigidbody.AddForce(thrusterDirection * thrusterForce);
+            fuel -= fuel_consumption * Time.deltaTime;
+        }
+
+        // Torque for A and D keys
+        Vector3 torque1 = rigidbody.transform.right * Input.GetAxis("Horizontal") * steeringForce;
+        // Torque for W and S keys
+        Vector3 torque2 = rigidbody.transform.forward * Input.GetAxis("Vertical") * steeringForce; 
+        // The sum of both inputs
+        rigidbody.AddTorque(torque1 + torque2);
+    }
+
+    void OnCollisionEnter(Collision collisionOBJ)
+    {
+        if (collisionOBJ.collider.CompareTag("Finish"))
+        {
+            Debug.Log("YOU WIN!");
+            UnityEngine.SceneManagement.SceneManager.LoadScene("Game");
+        }
+    }
+}
