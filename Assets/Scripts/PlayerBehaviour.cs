bf890d792ed30986194bbeaad0645209a5672b56
PixelZero <joris@lambooy.nl>
2017-03-23 19:33:32 +0100

    Changed the rocket model to indicate which direction is "Up". Also implemented a working Launch- / Fly-Phase system

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -6,8 +6,12 @@ public class PlayerBehaviour : MonoBehaviour
 {
     public Rigidbody playerRB;
 
+    public bool invertX;
+    public bool invertY;
+
     public float maxVelocity;
 
+    public float launchForce;
     public float thrusterForce;
     public float steeringForce;
     public float passiveCorrection;
@@ -15,43 +19,93 @@ public class PlayerBehaviour : MonoBehaviour
     public float fuel;
     public float fuel_consumption;
 
+    private enum GamePhase { Launch, Fly };
+    private GamePhase phase; 
+
+    void Start()
+    {
+        phase = GamePhase.Launch;
+    }
+
 	void Update ()
     {
-        // For now, just test the flying-phase
-        Phase_Fly();
+        switch (phase)
+        {
+            case GamePhase.Launch: Phase_Launch(); break;
+            case GamePhase.Fly: Phase_Fly(); break;
+        }
 	}
 
     private void Phase_Launch()
     {
-        // Still needs to be implemented properly
+        playerRB.useGravity = false;
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Vector3 thrustForce = playerRB.transform.up * thrusterForce * launchForce * Time.deltaTime * 100;
+            playerRB.AddForce(thrustForce);
+
+            playerRB.useGravity = true;
+            phase = GamePhase.Fly;
+        }
+
+        RotationControls();
     }
 
     private void Phase_Fly()
     {
         if (fuel > 0 && Input.GetKey(KeyCode.Space))
         {
-            Vector3 thrustForce = playerRB.transform.up * thrusterForce;
+            // Apply forward thrust
+            Vector3 thrustForce = playerRB.transform.up * thrusterForce * Time.deltaTime * 100;
             playerRB.AddForce(thrustForce);
+            fuel -= fuel_consumption * Time.deltaTime;
         }
 
+        // Calculate the angle between the direction the rocket is facing
+        // and the direction in which it is flying
         Vector3 currVelocity = playerRB.velocity;
         Vector3 currDirection = playerRB.transform.up * currVelocity.magnitude;
-        Vector3 correctionForce = (currDirection - currVelocity) * passiveCorrection * Time.deltaTime;
+        float angle = Vector3.Angle(currDirection, currVelocity);
 
-        //Vector3 force = playerRB.transform.up * passiveThrust;
-        //vel += new Vector3(0, -1, 0) * Gravity.gravityForce * Time.deltaTime;
+        // Apply the "correction force" only when the rocket is aligned properly ( -60° < angle < 60° )
+        // This prevents the correction force from acting as a "brake force", slowing down the fall of the rocket
+        if ( Mathf.Abs(angle) < 60 )
+        {
+            Vector3 correctionForce = (currDirection - currVelocity) * passiveCorrection * Time.deltaTime;
+            playerRB.AddForce(correctionForce);
+        }
 
-        playerRB.AddForce(correctionForce);
-        playerRB.AddForce(new Vector3(0, -1, 0) * Gravity.gravityForce * Time.deltaTime);
-        
-        //Debug.Log("current v: " + currVelocity.magnitude);
+        RotationControls();
+    }
 
+    // Method to handle the rotation of the rocket (WASD + Q,E Keys)
+    void RotationControls()
+    {
         // Torque for A and D keys
-        Vector3 torque1 = playerRB.transform.right * Input.GetAxis("Horizontal") * steeringForce;
+        Vector3 torque1 = -playerRB.transform.right * Input.GetAxis("Horizontal") * steeringForce;
+        if (invertX)
+            torque1 *= -1;
         // Torque for W and S keys
-        Vector3 torque2 = playerRB.transform.forward * Input.GetAxis("Vertical") * steeringForce; 
-        // The sum of both inputs
-        playerRB.AddTorque(torque1 + torque2);
+        Vector3 torque2 = -playerRB.transform.forward * Input.GetAxis("Vertical") * steeringForce;
+        if (invertY)
+            torque2 *= -1;
+        // Torque for Q and E keys
+        Vector3 torque3 = -playerRB.transform.up * Input.GetAxis("Roll") * steeringForce * 0.8f;
+
+        // The sum of all inputs
+        playerRB.AddTorque(torque1 + torque2 + torque3);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Ground"))
+        {
+            phase = GamePhase.Launch;
+            playerRB.useGravity = false;
+
+            playerRB.transform.position += new Vector3(0, 4, 0);
+            playerRB.velocity = Vector3.zero;
+        }
     }
 
     void OnTriggerEnter(Collider collider)
